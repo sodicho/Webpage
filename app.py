54869d39a42f0b7ebccc603b8d7a4a58e9bbf149
Sargon <Sodicho@iwu.edu>
2020-06-02 19:36:05 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/acting')
+def acting():
+	return render_template('acting.html', title = 'Acting')
+
+@app.route('/CS')
+def compSci():
+	return render_template('cs.html', title = 'CS Development')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html', title = 'Contact')
+
+if __name__ == '__main__':
+	app.run(debug = True)
