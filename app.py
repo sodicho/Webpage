3c94015bc58a121ac3b8de275ecc545adbfec6be
Sargon <Sodicho@iwu.edu>
2021-01-23 18:04:07 -0600

    Got rid of other pages

--- app.py
+++ app.py
@@ -4,18 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html')
-
-@app.route('/acting')
-def acting():
-	return render_template('home.html', title = 'Acting')
-
-@app.route('/CS')
-def compSci():
-	return render_template('home.html', title = 'CS Development')
-
-@app.route('/contact')
-def contact():
-	return render_template('home.html', title = 'Contact')
-
+    
 if __name__ == '__main__':
 	app.run(debug = True)
